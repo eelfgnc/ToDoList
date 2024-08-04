7f4a619c96131f7672af7e082348562c81e32ee4
eelfgnc <eelfgnc@gmail.com>
2024-08-04 09:07:15 +0300

    Kullanıcı yetkisi olmadığı bir sayfaya girdiğinde görebileceği sayfa oluşturuldu.

--- service/client/src/pages/FobiddenPage.tsx
+++ service/client/src/pages/FobiddenPage.tsx
@@ -1,6 +1,62 @@
+import {CardMedia, Grid, Paper, Typography } from "@mui/material";
+import AlertImages from "../assets/images/alert.png";
+import { Link } from "react-router-dom";
+
 const FobiddenPage = () => {
     return (
-        <>Yetkiniz yok</>
+        <Paper style={{	width: "50%",
+            minWidth: "500px",
+            padding: "40px",
+            marginTop: "32px",
+            marginBottom: "8px",
+            marginLeft: "auto",
+            marginRight: "auto"}}>
+            <Grid
+                container
+                spacing={1}
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                alignContent="center"
+            >
+                <Grid item xs={2}>
+                    <CardMedia
+                        component="img"
+                        sx={{
+                            width: 150,
+                            height: 150,
+                            margin: "20px auto",
+                            borderRadius: "0%",
+                        }}
+                        image={AlertImages}
+                        alt="App Logo"
+                    />
+                </Grid>
+                <Grid item xs={8}>
+                    <Grid>
+                        <Grid>
+                            <Typography variant="h5" align="center">You are not authorized to view this area!</Typography>
+                        </Grid>
+                        <Grid>
+                            <Link to={"http://localhost:3000/login"}><b>Return To Login Page!</b></Link>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={2}>
+                <CardMedia
+                        component="img"
+                        sx={{
+                            width: 150,
+                            height: 150,
+                            margin: "20px auto",
+                            borderRadius: "0%",
+                        }}
+                        image={AlertImages}
+                        alt="App Logo"
+                    />
+                </Grid>
+            </Grid>
+        </Paper>
     )
 };
 
